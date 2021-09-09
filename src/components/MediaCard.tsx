73df424a90a2a2aae2a6ee1366cd7a1415e976bd
Przemo199 <przemo199@onet.pl>
2021-09-09 04:43:17 +0100

    Started implementing media search

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -4,7 +4,7 @@ import {Card} from "react-bootstrap";
 
 function MediaCard(props: Resource): JSX.Element {
   return (
-    <Card style={{width: "10rem", backgroundColor: "rgb(35, 35, 35)"}}>
+    <Card className="media-card">
       {props.type.includes("video") && <video src={props.privateUrl} />}
       {props.type.includes("image") && <img alt={props.name} src={props.privateUrl} />}
       <Card.Title>{props.name}</Card.Title>
