49e75942fd8938b02cec377ad37af64a8c4297fe
Przemo199 <przemo199@onet.pl>
2021-09-10 01:20:47 +0100

    Started implementing media search

--- src/pages/MediaViewer.tsx
+++ src/pages/MediaViewer.tsx
@@ -1,49 +1,37 @@
 import React, {useEffect, useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {MediaResource} from "../interfaces";
 
 const apiGetEndpoint = "/api/get";
 
 function MediaViewer(): JSX.Element {
-  const path = useLocation().pathname;
-  // @ts-ignore
-  const {id} = useParams<string>();
+  const {id} = useParams<{id: string}>();
   const [resource, setResource] = useState<MediaResource | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchPrivateUrl() {
+    async function fetchMediaResource() {
       setIsLoading(true);
-      const resourceId = getResourceId();
-      if (resourceId) {
-        const request = await fetch(apiGetEndpoint, {
-          method: "POST",
-          body: JSON.stringify({"resourceId": id})
-        });
+      const request = await fetch(apiGetEndpoint, {
+        method: "POST",
+        body: JSON.stringify({"resourceId": id})
+      });
 
-        const response = await request.json();
-        const document = response.result;
+      const response = await request.json();
+      const document = response.result;
 
-        if (document) {
-          setResource(document);
-          setIsLoading(false);
-        }
+      if (document) {
+        setResource(document);
       }
+      setIsLoading(false);
     }
 
-    fetchPrivateUrl();
-  }, [path]);
+    fetchMediaResource();
+  }, [id]);
 
-  const getResourceId = (): string => {
-    const elems = path.split("/");
-    if (elems.length === 3) {
-      return elems[elems.length - 1];
-    }
-    return "";
-  };
-
-  const calculateISODate = (r: MediaResource): string => {
-    return (new Date(parseInt(r._id.toString().substring(0,8), 16) * 1000)).toISOString();
+  const getReadableDate = (resource: MediaResource): string => {
+    const date = (new Date(parseInt(resource._id.toString().substring(0,8), 16) * 1000)).toISOString();
+    return date.substring(0, 20).replace(/[A-Z]/, " ");
   };
 
   const calculateFileSize = (r: MediaResource): string  => {
@@ -61,15 +49,16 @@ function MediaViewer(): JSX.Element {
           <p className="inline">File name: {resource.name} </p>
           &nbsp;
           &nbsp;
-          <p className="inline">File type: {resource.type} </p><br />
+          <p className="inline">File type: {resource.type} </p>
+          <br />
           <p className="inline">File size: {calculateFileSize(resource)} </p>
           &nbsp;
           &nbsp;
-          <p className="inline">Upload timestamp: {calculateISODate(resource)} </p>
+          <p className="inline">Upload timestamp: {getReadableDate(resource)} </p>
         </div>
       }
       {!resource && !isLoading && <h1>It looks like the resource you are looking for does not exist</h1>}
-      {!resource && isLoading && <h1>Loading..</h1>}
+      {!resource && isLoading && <h1>Loading...</h1>}
     </React.Fragment>
   );
 }
